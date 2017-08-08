31816076da1dca255113963f09cfce88f15ab017
Martín Szyszlican <martinsz@gmail.com>
2017-08-08 03:05:01 -0300

    Fix for https in face2face and compass

--- votai_theme/static/js/main.js
+++ votai_theme/static/js/main.js
@@ -19,8 +19,8 @@ $(document).ready(function(){
 			var slug_two = $('#candidate_two').val();
 			var slug_one = $(this).val();
 			var data_path = path.split("/",4);
-			var new_url = 'http://' + host + '/' + data_path[1] + '/' + data_path[2] + '/' + data_path[3] + '/' + slug_one + '/' + slug_two;
-			
+			var new_url = '//' + host + '/' + data_path[1] + '/' + data_path[2] + '/' + data_path[3] + '/' + slug_one + '/' + slug_two;
+
 			window.location = new_url;
 		}
 	})
@@ -31,7 +31,7 @@ $(document).ready(function(){
 			var slug_one = $('#candidate_one').val();
 			var slug_two = $(this).val();
 			var data_path = path.split("/",4);
-			var new_url = 'http://' + host + '/' + data_path[1] + '/' + data_path[2] + '/' + data_path[3] + '/' + slug_one + '/' + slug_two;
+			var new_url = '//' + host + '/' + data_path[1] + '/' + data_path[2] + '/' + data_path[3] + '/' + slug_one + '/' + slug_two;
 
 			window.location = new_url;
 		}
